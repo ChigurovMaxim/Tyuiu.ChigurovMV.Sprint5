667ee30f5cc93ab8684a098aa55567f1126eac55
Tyuiu.ChigurovMV@outlook.com <maxchigur@mail.ru>
2024-11-26 12:49:50 +0500

    Task4

--- Tyuiu.ChigurovMV.Sprint5.Task4.V25.Lib/DataService.cs
+++ Tyuiu.ChigurovMV.Sprint5.Task4.V25.Lib/DataService.cs
@@ -7,14 +7,13 @@ namespace Tyuiu.ChigurovMV.Sprint5.Task4.V25.Lib
     public class DataService : ISprint5Task4V25
     {
         public double LoadFromDataFile(string path)
-        {      
-            string strX = File.ReadAllText(path);
-            NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+        {
+            NumberFormatInfo info = new NumberFormatInfo()
             {
-                NumberDecimalSeparator = ",",
+                NumberDecimalSeparator = "."
             };
-
-            double x = double.Parse(strX, numberFormatInfo);
+            string strX = File.ReadAllText(path);
+            double x = double.Parse(strX,info);
             double res = Math.Round(((Math.Pow(x, 4) + Math.Cos(x)) * Math.Sin(x)), 3);
             return res;
         }
