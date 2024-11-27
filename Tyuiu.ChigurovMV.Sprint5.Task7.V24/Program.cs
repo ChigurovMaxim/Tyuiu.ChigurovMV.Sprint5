80d83eb21fcb97ddcba929dab10cdd85b0f80c7f
Tyuiu.ChigurovMV@outlook.com <maxchigur@mail.ru>
2024-11-27 11:55:08 +0500

    Task7

--- Tyuiu.ChigurovMV.Sprint5.Task7.V24/Program.cs
+++ Tyuiu.ChigurovMV.Sprint5.Task7.V24/Program.cs
@@ -21,13 +21,14 @@ namespace Tyuiu.ChigurovMV.Sprint5.Task7.V24
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
-            string path = @"C:\DataSprint5\InPutDataFileTask7V24.txt";
+            string path = @"C:\DataSprint5\InPutFileTask7V24.txt";
+            string pathSaveFile = @"C:\DataSprint5\OutPutFileTask7V24.txt";
             Console.WriteLine("Данные находятся в файле: " + path);
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("Находится в файле: ");
-            string pathSaveFile = ds.LoadDataAndSave(path);
+            pathSaveFile = ds.LoadDataAndSave(path);
             Console.WriteLine(pathSaveFile);
         }
     }
